e0156821d40cf2bbe1290f8123532dcccb1832bc
aditiadhikarii <adhiaditi10@gmail.com>
2023-09-19 17:36:43 +0545

    add : first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// let a = 1234
+// Cconsole.log(typeof(a))
+// let b = a.toString()
+// let x = "1234"
+// let y = parcetInt(x)
+// let z = parcentfloat(x)
+// console.log(typeof(x))
+// console.log(typeof(y))
+
+function myFunction(){
+    a = prompt("Enter a string ");
+    b = a.toUpperCase();
+    document.getElementById("header1").innerHTML = b;
+}
+
+// let input = prompt("Enter a sting: ");
+// input = input.toUpperCase();
+// document.querySelector("#header2")
+
+function count(sentence,countInput){
+    let sentenceArray = sentence.split(" ");
+    
+}
